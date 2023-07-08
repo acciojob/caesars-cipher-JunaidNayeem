7e0fb89aa19001dc94e57466aeb3b9aec7986e46
JunaidNayeem <78254107+JunaidNayeem@users.noreply.github.com>
2023-07-08 23:37:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -66,6 +66,7 @@ function rot13(encodedStr) {
   // Return the decoded string
   return decodedString;
 	console.log(decodedString);
+	window.show(decodedString);
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
